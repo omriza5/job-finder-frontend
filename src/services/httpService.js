b98ee2379a1e2d52de7fd5884e45b36a9c49c221
omri zaher <omriza5@gmail.com>
2021-11-26 18:22:31 +0200

    add: http service using axios

---
+++ src/services/httpService.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    toast.error("🦄 Wow so easy!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  return Promise.reject(error);
+});
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
+export default http;
